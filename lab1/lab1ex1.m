18a93602f11f785a8f32d6b7ebf530e47276a67a
Paolo Grasso <paolo.1995@ymail.com>
2019-05-07 10:54:53 +0200

    Update file

--- lab1/lab1ex1.m
+++ lab1/lab1ex1.m
@@ -1,7 +1,7 @@
 % Code for tasks 1-2
 
-clc; clear; close all;
-format long
+clc; clear; %close all;
+format short
 
 % NominalUEREfixed.
 P1n = load('dataset_CPT_20180328T122038');
@@ -19,9 +19,17 @@ P4r = load('dataset_SHA_20180329T161103');
 P5r = load('dataset_STA_20180329T161418');
 P6r = load('dataset_TRN_20180329T161139');
 
+% Realistic 2019
+% test1 = load('dataset_1_20180329T160947');
+% test2 = load('dataset_2_20180329T160900');
+% test3 = load('dataset_3_20180329T161023');
+% test4 = load('dataset_4_20180329T161103');
+% test5 = load('dataset_5_20180329T161418');
+% test6 = load('dataset_CPT_20180329T160947');
+
 place = '     Point';  % Label for world plot
 
-P = P2r
+P = P6n;
 
 cnt = 0;
 rhoMin = NaN;
@@ -29,12 +37,12 @@ rhoMax = NaN;
 minCor = [NaN NaN];
 maxCor = [NaN NaN];
 for set =  ["GPS", "GAL", "GLO", "BEI"]
-rhoMin = min(rhoMin, min(min(P.RHO.(set))));
-rhoMax = max(rhoMax, max(max(P.RHO.(set))));
+% rhoMin = min(rhoMin, min(min(P.RHO.(set))));
+% rhoMax = max(rhoMax, max(max(P.RHO.(set))));
 cnt = cnt + 1;
     
 time = length(P.RHO.(set)(1,:));
-xTicks = [0 : 900 : time];
+xTicks = 0 : 900 : time;
 
 sat = zeros(1,time);
 for s = 1 : time
@@ -43,7 +51,8 @@ end
 
 %% Recursive Least Mean Square
 
-L = length(P.RHO.(set)(:,1))
+L = length(P.RHO.(set)(:,1));
+sigmaUERE = zeros(1, L);
 for c = 1 : L
     r = P.RHO.(set)(c,:);
     r = r(~(isnan(r)));
@@ -69,7 +78,7 @@ for n = 1 : time  % time instants
         for s = 1 : length(visSat)  % Iterations on satellites
             e = P.SAT_POS_ECEF.(set)(visSat(s)).pos(n,:) - xHat(1:3);
             rhoHat(s) = sqrt(sum(e.^2));
-            H(s,1:3) = [e/rhoHat(s)];
+            H(s,1:3) = e/rhoHat(s);
         end  
         
         % Step 1: DeltaRhoHat  
@@ -93,15 +102,20 @@ for n = 1 : time  % time instants
     clear xHat
 end
 
+% posError = std(pos);
+% fprintf("LMS sigmaX for %s =\n", set)
+% display(posError')
+fprintf("LMS sigmaX for %s = %.4f \n", set, sqrt(trace(cov(pos(1:3, 1:3)))))
+lmsSigmaX(cnt) = sqrt(trace(cov(pos(1:3, 1:3))));
 for tPos = 1 : length(pos)
     coord(tPos, 1:3) = ecef2lla(pos(tPos, 1:3));  %coord è in lon,lat,alt
 end
 
-
+mPdop(cnt,1) = mean(pdop);
 
 % Save position for Google Earth
-filename = '.\lab1\Helsinki.m';
-writeKML_GoogleEarth(filename, coord(:, 1), coord(:, 2), coord(:, 3))
+% filename = '.\lab1\Helsinki.m';
+% writeKML_GoogleEarth(filename, coord(:, 1), coord(:, 2), coord(:, 3))
 
 %plot(errors)
 %H2=H(1:end-1,1:end-1)
@@ -114,6 +128,9 @@ writeKML_GoogleEarth(filename, coord(:, 1), coord(:, 2), coord(:, 3))
 k = 1;
 satPlot = zeros(6, time);
 for PS = [P1n, P2n, P3n, P4n, P5n, P6n]
+%for PS = [test1, test2, test3, test4, test5, test6]
+%for PS = [testerer]
+%for PS = [P1r, P2r, P3r, P4r, P5r, P6r]
     
     for s = 1 : time
         satPlot(k,s) = sum(not(isnan(PS.RHO.(set)(:,s))));
@@ -126,8 +143,11 @@ hold on
 plot(satPlot', 'linewidth', 1.5)
 xlabel("Time")
 ylabel("No. of visible satellites")
-title([set])%, "No. of visible satellites over time"])
-ylim([5 20])
+title(set)%, "No. of visible satellites over time"])
+%ylim([5 20])
+if set == 'GPS'
+    ylim([0 24])
+end
 % ylim([min(sat)-1, max(sat)+1])
 xlim([0,time])
 xticks(xTicks);
@@ -140,15 +160,26 @@ subplot(2, 2, cnt)
 plot(P.RHO.(set)', 'linewidth', 1)
 xlabel("Time")
 ylabel("Pseudorange [m]")
-title([set])
+title(set)
 xlim([0,time])
 xticks(xTicks);
 grid on
 grid minor
 
 %% Plot: World Map Plot
-figure(3)
-WorldMapPlotter(coord(time,1), coord(time,2), place, 'World')
+% figure(3)
+% hold on
+% WorldMapPlotter(coord(time,1), coord(time,2), place, 'World')
+
+
+% figure(99)
+% hold on
+% lat = coord(time,1);
+% lon = coord(time,2);
+% name = '      TRN';
+% plotm(lat,lon,'ro','MarkerSize',10)
+% plotm(lat,lon,'r+','MarkerSize',30,'LineWidth',1)
+% textm(lat,lon,name)
 
 %% Plot: Scatter plot of positions
 % Real position is the mean of all positions.
@@ -160,12 +191,12 @@ errors = realPos - pos(:,1:3);
 %posLla = ecef2lla(pos(:,1:3))
 %errors = realPosLla - posLla(:,1:3);
 %sigmaX = sqrt(trace(cov(errors)));
-fprintf("sigmaX = %.6f\n", sigmaX)
+% fprintf("sigmaX = %.6f\n", sigmaX);
 fprintf("Avg. PDOP = %.6f metres\n", mean(pdop))
 fprintf("Avg. GDOP = %.6f metres\n", mean(gdop))
 fprintf("Avg. sigmaUERE = %.6f\n", nanmean(sigmaUERE))
 res = mean(gdop)*(nanmean(sigmaUERE));
-fprintf("PDOP * sigmaUERE = %.6f\n", res)
+fprintf("PDOP * sigmaUERE = %.6f\n", res);
 
 figure(4)
 subplot(2, 4, cnt)
@@ -174,15 +205,17 @@ minCor(1) = min(minCor(1), min(coord(:,1)));
 minCor(2) = min(minCor(2), min(coord(:,2)));
 maxCor(1) = max(maxCor(1), max(coord(:,1)));
 maxCor(2) = max(maxCor(2), max(coord(:,2)));
-geoshow(coord(:,1), coord(:,2),'DisplayType', 'point', 'Marker', '.')
+geoshow(coord(:,1), coord(:,2),'DisplayType', 'point', 'Marker', '.',...
+        'MarkerEdgeColor', 	[0.6350, 0.0780, 0.1840])
 geoshow(realPosLla(1), realPosLla(2),'DisplayType', 'point',...
-        'MarkerEdgeColor','blue', 'Marker','o', 'MarkerFaceColor', 'green')
+        'MarkerEdgeColor','black', 'Marker','o',...
+        'MarkerFaceColor', [0.4660, 0.6740, 0.1880])
 legend('Estimated', 'Real')
 xlabel('Longitude')
 ylabel('Latitude')
 %xlim([24.936 24.94])
 %ylim([60.1696 60.17])
-title([set])%'Positions')
+title(set)%'Positions')
 axis equal; axis square; grid on;
 
 
@@ -204,7 +237,7 @@ hold on
 % plot(gdop)
 plot(gdop*nanmean(sigmaUERE))
 plot(sigmaX)
-title([set]) %"Dilution of precision")
+title(set) %"Dilution of precision")
 grid minor
 xlabel('Time')
 %ylabel('PDOP')
@@ -212,7 +245,7 @@ xlim([0,time])
 xticks(xTicks);
 legend('GDOP \cdot \sigma_{UERE}', '\sigma_X', 'location', 'best')
 
-std(errors)
+std(errors);
 clear R  sigmaUERE sigmaX H G
 end
 
@@ -237,17 +270,17 @@ end
 
 
 
-P = P2r
+% P = P1r;
 cnt = 0;
 rhoMin = NaN;
 rhoMax = NaN;
 for set =  ["GPS", "GAL", "GLO", "BEI"]
-rhoMin = min(rhoMin, min(min(P.RHO.(set))));
-rhoMax = max(rhoMax, max(max(P.RHO.(set))));
+% rhoMin = min(rhoMin, min(min(P.RHO.(set))));
+% rhoMax = max(rhoMax, max(max(P.RHO.(set))));
 cnt = cnt + 1;
     
 time = length(P.RHO.(set)(1,:));
-xTicks = [0 : 900 : time];
+xTicks = 0 : 900 : time;
 
 sat = zeros(1,time);
 for s = 1 : time
@@ -255,7 +288,8 @@ for s = 1 : time
 end
 
 %% WLMS
-L = length(P.RHO.(set)(:,1))
+L = length(P.RHO.(set)(:,1));
+sigmaUERE = zeros(1, L);
 for c = 1 : L
     r = P.RHO.(set)(c,:);
     r = r(~(isnan(r)));
@@ -281,7 +315,7 @@ for n = 1 : time  % time instants
         for s = 1 : length(visSat)  % Iterations on satellites
             e = P.SAT_POS_ECEF.(set)(visSat(s)).pos(n,:) - xHat(1:3);
             rhoHat(s) = sqrt(sum(e.^2));
-            H(s,1:3) = [e/rhoHat(s)];
+            H(s,1:3) = e/rhoHat(s);
         end  
         
         % Step 1: DeltaRhoHat  
@@ -289,18 +323,18 @@ for n = 1 : time  % time instants
   
         % Step 2: H_w
         W = inv(R(visSat, visSat));
-        H_w = inv(H.'*W*H)*H.'*W;
+        H_w = inv(H.'*W*H)*(H.')*W;
+        
         % Step 3: DeltaXHat
         eXHat = H_w * eRhoHat; 
-        
+       
         % Step 4: xHat
         xHat = xHat + eXHat.';
                 
-        
     end
     
     H_w = H_w.';
-    G = inv(H.'*H);
+    G = inv(H_w.'*H_w);
     pdop(n) = sqrt(trace(G(1:end-1, 1:end-1)));
     gdop(n) = sqrt(trace(G));
     sigmaX(n) = sqrt(trace(inv(H.'*H) * H.'*R(visSat, visSat)*H*inv(H.'*H)));
@@ -310,10 +344,15 @@ for n = 1 : time  % time instants
     clear xHat
 end
 
+fprintf("WLMS sigmaX for %s =\n", [set])
+% display(posError')
+% display(sqrt(sum(posError.^2))')
+
 for tPos = 1 : length(pos)
     coord(tPos, 1:3) = ecef2lla(pos(tPos, 1:3));  %coord è in lon,lat,alt
 end
 
+mPdop(cnt,2) = mean(pdop);
 
 %% Plot: Visible satellites vs time.
 % figure(11)
@@ -342,6 +381,7 @@ grid minor
 
 %% Plot: World Map Plot
 figure(13)
+hold on
 WorldMapPlotter(coord(time,1), coord(time,2), place, 'World')
 
 %% Plot: Scatter plot of positions
@@ -351,21 +391,24 @@ WorldMapPlotter(coord(time,1), coord(time,2), place, 'World')
 % errors = realPos - pos(:,1:3);
 % sigmaX = sqrt(trace(cov(errors)));
 
-fprintf("Avg. PDOP = %.2d metres\n", mean(pdop))
-fprintf("Error: %.2d metres\n", sigmaX)
-fprintf("Avg. sigmaUERE = %.2d\n", nanmean(sigmaUERE))
-mean(pdop)*(nanmean(sigmaUERE))
+fprintf("Avg. PDOP = %.6f metres\n", mean(pdop))
+fprintf("Avg. GDOP = %.6f metres\n", mean(gdop))
+% fprintf("Error: %.2d metres\n", sigmaX);
+fprintf("Avg. sigmaUERE = %.2d\n", nanmean(sigmaUERE));
+mean(pdop)*(nanmean(sigmaUERE));
 
 figure(4)
 subplot(2, 4, cnt+4)
 hold on
-geoshow(coord(:,1), coord(:,2),'DisplayType', 'point', 'Marker', '.')
+geoshow(coord(:,1), coord(:,2),'DisplayType', 'point', 'Marker', '.',...
+        'MarkerEdgeColor', [0, 0.4470, 0.7410])
 geoshow(realPosLla(1), realPosLla(2),'DisplayType', 'point',...
-        'MarkerEdgeColor','blue', 'Marker','o', 'MarkerFaceColor', 'green')
+        'MarkerEdgeColor', 'black', 'Marker','o',...
+        'MarkerFaceColor', [0.8500, 0.3250, 0.0980])
 legend('Estimated', 'Real')
 xlabel('Longitude')
 ylabel('Latitude')
-title([set])%'Positions')
+title(set)%'Positions')
 axis equal; axis square; grid on;
 
 
@@ -375,7 +418,7 @@ subplot(2, 2, cnt)
 hold on
 plot(gdop)
 plot(sigmaX)
-title([set])%"Dilution of precision")
+title(set)%"Dilution of precision")
 legend('GDOP', '\sigma_X', 'location', 'best')
 xlabel('Time')
 % ylabel('PDOP')
@@ -393,10 +436,11 @@ end
 
 
 figure(4)
-%linkaxes([subplot(2,4,1),subplot(2,4,2),subplot(2,4,3),subplot(2,4,4),...
-%    subplot(2,4,5),subplot(2,4,6),subplot(2,4,7),subplot(2,4,8)],'xy')
-linkaxes([subplot(2,4,1),subplot(2,4,2),subplot(2,4,3),...
-    subplot(2,4,5),subplot(2,4,6),subplot(2,4,7)],'xy')
+linkaxes([subplot(2,4,1),subplot(2,4,2),subplot(2,4,3),subplot(2,4,4),...
+    subplot(2,4,5),subplot(2,4,6),subplot(2,4,7),subplot(2,4,8)],'xy')
+%linkaxes([subplot(2,4,1),subplot(2,4,2),subplot(2,4,3),...
+%    subplot(2,4,5),subplot(2,4,6),subplot(2,4,7)],'xy')
 
 % figure(14)
 % linkaxes([subplot(2,2,1),subplot(2,2,2),subplot(2,2,3),subplot(2,2,4)],'xy')
+
