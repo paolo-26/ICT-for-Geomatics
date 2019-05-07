f38a3664a46917670ec76d665d6df1788681a3f8
Paolo Grasso <paolo.1995@ymail.com>
2019-05-07 16:15:40 +0200

    Update file

--- lab2/lab2ex2.m
+++ lab2/lab2ex2.m
@@ -1,19 +1,19 @@
 close all; clc; clear all;
 
-% Satellite position
+% Satellite position.
 X = 15487292.829;
-Y = 6543538.932;
+Y =  6543538.932;
 Z = 20727274.429;
 
 % Parameters.
-a = 6378137.0;  % Semi-major axis
+a = 6378137;  % Semi-major axis
 f = 1 / 298.257223;  % Flattening
 
 % Station coordinates.
-phi = 45.06336;
-lambda = 7.661279;
-phi = deg2rad(phi);
-lambda = deg2rad(lambda);
+phi = dms2degrees([45, 3, 48.114]);  % Latitude in degrees
+lambda = dms2degrees([7, 39, 40.605]);  % Longitude in degrees
+phi = deg2rad(phi);  % Latitude in radiants
+lambda = deg2rad(lambda);  % Longitude in radiants
 
 e2 = (2*f - f^2);
 W = sqrt(1 - e2*(sin(phi))^2);
@@ -21,21 +21,22 @@ W = sqrt(1 - e2*(sin(phi))^2);
 % Station position.
 Xi = a * cos(phi) * cos(lambda) / W;
 Yi = a * cos(phi) * sin(lambda) / W;
-Zi = a * (1-e2) * sin(phi) / W;
+Zi = a * (1-e2)   * sin(phi)    / W;
 
 
-R = [-sin(lambda), cos(lambda), 0;...
-     -sin(phi)*cos(lambda), -sin(phi)*sin(lambda), cos(phi);...
-     cos(phi)*cos(lambda), cos(phi)*sin(lambda), sin(phi)];
+R = [-sin(lambda),           cos(lambda),            0;...
+     -sin(phi)*cos(lambda),  -sin(phi)*sin(lambda),  cos(phi);...
+      cos(phi)*cos(lambda),  cos(phi)*sin(lambda),   sin(phi)];
 M = [X-Xi; Y-Yi; Z-Zi];
-res = R * M;
+enu = R * M;
 
 % Local coordinates.
-e = res(1)
-n = res(2)
-u = res(3)
- 
-azimuth = rad2deg(atan(u / sqrt(n^2 + e^2)))
-elevation = rad2deg(atan(e / n))
+e = enu(1)
+n = enu(2)
+u = enu(3)
+azimuth = atan(u / sqrt(n^2 + e^2));  % Azimuth in radiants
+azimuth = rad2deg(azimuth)  % Azimuth in degrees.
+elevation = atan(e / n);  % Azimuth in radiants
+elevation = rad2deg(elevation)  % Elevation in degrees.
 
            
