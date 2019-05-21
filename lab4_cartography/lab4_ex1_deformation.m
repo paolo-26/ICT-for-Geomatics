0bf1fac751de1bf4a7f740f2cbb6d7d35c37de28
Paolo Grasso <paolo.1995@ymail.com>
2019-05-21 15:15:55 +0200

    Fix bug and update graph

--- lab4_cartography/lab4_ex1_deformation.m
+++ lab4_cartography/lab4_ex1_deformation.m
@@ -1,6 +1,6 @@
 close all; clear all; clc;
 
-phi = deg2rad([0 30 37 45 60]);
+phi = deg2rad([30 37 45 60]);
 lambda = deg2rad([0 : 0.5 : 3]);
 
 figure(1)
@@ -8,16 +8,12 @@ hold on
 grid on
 for k = 1 : length(phi)
     m = 0.9996*(1 + (((lambda.^2)./2).*(cos(phi(k))).^2));
-
-    plot([0 : 0.5 : 3], m, '.-', 'linewidth',2, 'Markersize', 20)
-    if k == 1
-        yticks(m)
-    end
+    plot([0 : 0.5 : 3], m, 'o-', 'linewidth',2)
 end
 
-legend("equator", "case 1", "case 2", "case 3", "case 4",...
-       'location', 'best');
+legend("case 1", "case 2", "case 3", "case 4", 'location','northwest');
 ylabel("\it m_l");
+yticks([0.9996 : 0.0001 : 1.0008])
 xlabel("longitude");
 xtickformat('degrees');
 grid minor
